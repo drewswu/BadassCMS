8497dc9eb095e57cd717914babe9bdb5e15a4a20
Andrew Wu <awu@twitter.com>
2013-04-28 10:47:33 +0100

    added firewall rules to route 80 to 8080

--- install.sh
+++ install.sh
@@ -16,9 +16,10 @@ function die() {
 }
 
 function route_firewall() {
-  sudo service iptables stop
   sudo iptables -X
-#  sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j     REDIRECT --to-ports 8080
+  sudo iptables -A INPUT -i eth0 -p tcp --dport 80 -j ACCEPT
+  sudo iptables -A INPUT -i eth0 -p tcp --dport 8080 -j ACCEPT
+  sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
 }
 
 function install_admin_pkgs() {
