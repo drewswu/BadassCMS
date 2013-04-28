53db4ff7f52f33d6a6553280780ed12096321e9d
Andrew Wu <awu@twitter.com>
2013-04-28 10:49:26 +0100

    added firewall rules to route 80 to 8080

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ function die() {
 }
 
 function route_firewall() {
-  sudo iptables -X
+  sudo iptables --flush
   sudo iptables -A INPUT -i eth0 -p tcp --dport 80 -j ACCEPT
   sudo iptables -A INPUT -i eth0 -p tcp --dport 8080 -j ACCEPT
   sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
